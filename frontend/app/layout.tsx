9a8b82cbc241b5fc66f26df98ef8c0a26cd305ee
Jingmozhiyu <keqinglee000@gmail.com>
2026-08-10 18:12:50 +0800

    optimize font pack size

--- frontend/app/layout.tsx
+++ frontend/app/layout.tsx
@@ -9,12 +9,12 @@ import {getServerSession} from '@/lib/auth/session.server'
 
 const hanyiWenHei = localFont({
     src: [
-        {path: '../public/汉仪文黑-35W.ttf', weight: '300', style: 'normal'},
-        {path: '../public/汉仪文黑-45W.ttf', weight: '400', style: 'normal'},
-        {path: '../public/汉仪文黑-55W.ttf', weight: '500', style: 'normal'},
-        {path: '../public/汉仪文黑-65W.ttf', weight: '600', style: 'normal'},
-        {path: '../public/汉仪文黑-75W.ttf', weight: '700', style: 'normal'},
-        {path: '../public/汉仪文黑-85W.ttf', weight: '800', style: 'normal'},
+        {path: '../public/hanyi-wenhei-35w-latin.woff2', weight: '300', style: 'normal'},
+        {path: '../public/hanyi-wenhei-45w-latin.woff2', weight: '400', style: 'normal'},
+        {path: '../public/hanyi-wenhei-55w-latin.woff2', weight: '500', style: 'normal'},
+        {path: '../public/hanyi-wenhei-65w-latin.woff2', weight: '600', style: 'normal'},
+        {path: '../public/hanyi-wenhei-75w-latin.woff2', weight: '700', style: 'normal'},
+        {path: '../public/hanyi-wenhei-85w-latin.woff2', weight: '800', style: 'normal'},
     ],
     display: 'swap',
     preload: false,
